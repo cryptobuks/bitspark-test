10fb26f0d7e66ce9836602005727a0194eff32d0
Marian Schubert <marian.schubert@gmail.com>
2018-02-25 15:38:41 +0100

    Make easy to run both UI & server in dev-friendly mode
    make -C web-wallet dev

--- web-wallet/server/server.js
+++ web-wallet/server/server.js
@@ -4,5 +4,5 @@ const app = express()
 app.use(express.static(process.env.PUBLIC_DIR || '../ui/dist'))
 app.get('/api', (req, res) => res.send('API'))
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 8081
 app.listen(port, () => console.log('Running at http://localhost:' + port))
