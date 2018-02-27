42e78de4e0e3e97524ceb3d5c2e4db0d69429a1f
Kováč Peter <kovac@avast.com>
2018-02-27 08:52:56 +0100

    Added stop scripts

---
+++ scripts/stopbtcd_testnet.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./mnt/data/bitcoin/bitcoinbin/bin/bitcoin-cli -testnet -rpcuser=bitspark -rpcpassword=bitspark stop
---
+++ scripts/stoplnd_testnet.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./mnt/data/lightning/lightning/cli/lightning-cli --lightning-dir=/mnt/data/lightning/data_testnet stop
