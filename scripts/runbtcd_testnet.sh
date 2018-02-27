aafbcab6822f7e2a6c2fb5d3ddd5959a56bb9d09
Kováč Peter <kovac@avast.com>
2018-02-27 08:37:28 +0100

    Absolute path in start/stop scripts.

--- scripts/runbtcd_testnet.sh
+++ scripts/runbtcd_testnet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bitcoind -testnet -datadir=/mnt/data/bitcoin/data_testnet -server=1 -daemon=1 -par=2 -txindex=1 -rpcuser=bitspark -rpcpassword=bitspark
+./mnt/data/bitcoin/bitcoinbin/bin/bitcoind -testnet -datadir=/mnt/data/bitcoin/data_testnet -server=1 -daemon=1 -par=2 -txindex=1 -rpcuser=bitspark -rpcpassword=bitspark
--- scripts/runlnd_testnet.sh
+++ scripts/runlnd_testnet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./lightningd/lightningd --daemon --bitcoin-datadir /mnt/data/bitcoin/data_testnet --alias bsp1 --lightning-dir /mnt/data/lightning/data_testnet --network testnet --bitcoin-rpcuser bitspark --bitcoin-rpcpassword bitspark
+./mnt/data/lightning/lightning/lightningd/lightningd --daemon --bitcoin-datadir /mnt/data/bitcoin/data_testnet --alias bsp1 --lightning-dir /mnt/data/lightning/data_testnet --network testnet --bitcoin-rpcuser bitspark --bitcoin-rpcpassword bitspark
