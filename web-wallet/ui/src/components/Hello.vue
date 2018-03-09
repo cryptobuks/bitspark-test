0bb9bd3c971e92d8743759bb2a5468cb63b9eaca
Marian Schubert <marian.schubert@gmail.com>
2018-03-09 18:28:15 +0100

    Improve layout of welcome screen

--- web-wallet/ui/src/components/Hello.vue
+++ web-wallet/ui/src/components/Hello.vue
@@ -1,12 +1,17 @@
 <template>
-  <v-container fill-height justify-center>
-    <LoginButton v-if="!user" />
-    <div v-else>
-      <h1 class="mb-2">Welcome Your Wallet</h1>
-      <p>
-        Balance: <Amount :msatoshi="balance && balance.msatoshi" />
-      </p>
-    </div>
+  <v-container fill-height>
+    <v-layout align-center justify-center text-xs-center>
+      <v-flex xs12>
+        <LoginButton v-if="!user" />
+        <div v-else>
+          <h1 class="mb-5">Welcome Your Wallet</h1>
+          <p>
+            Available balance<br />
+            <strong><Amount :msatoshi="balance && balance.msatoshi" /></strong>
+          </p>
+        </div>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
