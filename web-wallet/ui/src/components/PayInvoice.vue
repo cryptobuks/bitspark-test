95db429e9862184d5edf36d81c8d89d6837817d0
Marian Schubert <marian.schubert@gmail.com>
2018-03-10 14:36:32 +0100

    Make payment screen look good on large displays

--- web-wallet/ui/src/components/PayInvoice.vue
+++ web-wallet/ui/src/components/PayInvoice.vue
@@ -17,36 +17,40 @@
       </div>
       <!-- Show invoice -->
       <v-container v-else fluid>
-        <v-card>
-          <v-card-title class="grey lighten-4 py-0">
-            <v-container class="pa-0">
-              <v-layout row text-xs-center>
-                <v-flex xs10 offset-xs1>
-                  <v-chip small label class="mt-0 grey lighten-5">Lightning Payment</v-chip>
-                </v-flex>
-              </v-layout>
-              <v-layout row text-xs-center class="my-4">
-                <v-flex xs12>
-                  <Amount :msatoshi="invoicePayload.msatoshi" />
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card-title>
-          <v-card-text>
-            {{ invoicePayload.description }}
-          </v-card-text>
-          <v-footer height="auto" class="transparent mt-5">
-            <v-layout row>
-              <v-flex xs6 text-xs-right class="pr-3">
-                <v-btn flat color="grey lighten-2" v-on:click="cancelPayment">Cancel</v-btn>
-              </v-flex>
-              <v-flex xs6 text-xs-left class="pl-3">
-                <LoginButton v-if="!user" />
-                <v-btn v-else flat color="primary" v-on:click="processPayment">&nbsp;<v-icon left class="mr-2">mdi-check-circle</v-icon>Pay&nbsp;</v-btn>
-              </v-flex>
-            </v-layout>
-          </v-footer>
-        </v-card>
+        <v-layout row justify-center>
+          <v-flex xs12 sm6 lg4>
+            <v-card>
+              <v-card-title class="grey lighten-4 py-0">
+                <v-container class="pa-0">
+                  <v-layout row text-xs-center>
+                    <v-flex xs10 offset-xs1>
+                      <v-chip small label class="mt-0 grey lighten-5">Lightning Payment</v-chip>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout row text-xs-center class="my-4">
+                    <v-flex xs12>
+                      <Amount :msatoshi="invoicePayload.msatoshi" />
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card-title>
+              <v-card-text>
+                {{ invoicePayload.description }}
+              </v-card-text>
+              <v-footer height="auto" class="transparent mt-5">
+                <v-layout row>
+                  <v-flex xs6 text-xs-right class="pr-3">
+                    <v-btn flat color="grey lighten-2" v-on:click="cancelPayment">Cancel</v-btn>
+                  </v-flex>
+                  <v-flex xs6 text-xs-left class="pl-3">
+                    <LoginButton v-if="!user" />
+                    <v-btn v-else flat color="primary" v-on:click="processPayment">&nbsp;<v-icon left class="mr-2">mdi-check-circle</v-icon>Pay&nbsp;</v-btn>
+                  </v-flex>
+                </v-layout>
+              </v-footer>
+            </v-card>
+          </v-flex>
+        </v-layout>
       </v-container>
     </v-flex>
   </v-layout>
