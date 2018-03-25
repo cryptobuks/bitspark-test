cd172f3b99692e9fcf24bae99ec095d69ecb4c7e
Marian Schubert <marian.schubert@gmail.com>
2018-03-25 14:20:33 +0200

    Use async/await when processing payment
    It's easier to reason about then with promises

--- web-wallet/server/src/payment-routes.js
+++ web-wallet/server/src/payment-routes.js
@@ -38,48 +38,44 @@ router.get('/invoice/info', (req, res) => {
     })
 })
 
-router.post('/invoice/pay', jwtCheck, (req, res) => {
+router.post('/invoice/pay', jwtCheck, async (req, res) => {
   console.info('PAY', req.body)
-
   var invoice = req.body.invoice
-  if (!validateInvoice(invoice)) {
+
+  try {
+    if (!validateInvoice(invoice)) {
+      throw new Error('Missing invoice parameter')
+    }
+
+    // Get invoice payload
+    const invoicePayload = await lightning.getInvoicePayload(invoice)
+
+    // Try to deduct amount from wallet (throws on insufficient funds)
+    console.log('PAY - Deduct', invoicePayload.msatoshi)
+    await db.updateWalletBalance(req.user, -invoicePayload.msatoshi)
+
+    // Try to process payment (and restore wallet balance if it fails)
+    try {
+      console.log('PAY - processPayment')
+      const result = await lightning.payInvoice(invoice)
+      console.log('PAY_RESULT', payload)
+      res.json({
+        status: 'OK',
+        payload
+      })
+      return
+    } catch (err) {
+      console.log('PAY - Restore', invoicePayload.msatoshi)
+      db.updateWalletBalance(req.user, invoicePayload.msatoshi)
+      throw err
+    }
+  } catch (err) {
+    console.error('/invoice/pay', err)
     res.json({
       status: 'ERROR',
-      error: 'Missing invoice parameter'
+      error: err.message
     })
-
-    return
   }
-
-  var processPayment = () => lightning.payInvoice(invoice).then(payload => {
-    console.log('PAY_RESULT', payload)
-    res.json({
-      status: 'OK',
-      payload
-    })
-  })
-
-  return lightning.getInvoicePayload(invoice)
-    .then(invoice => {
-      console.log('PAY - Deduct', invoice.msatoshi)
-      return db.updateWalletBalance(req.user, -invoice.msatoshi)
-        .then(() => {
-          console.log('PAY - processPayment')
-          return processPayment()
-            .catch(err => {
-              console.log('PAY - Restore', invoice.msatoshi)
-              db.updateWalletBalance(req.user, invoice.msatoshi)
-              throw err
-            })
-          })
-    })
-    .catch(e => {
-      console.error('/invoice/pay', e)
-      res.json({
-        status: 'ERROR',
-        error: e.message
-      })
-    })
-})
+ })
 
 module.exports = router
