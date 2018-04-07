8e5a9bddfb7bdc65049ce124a14af97bd06cfb5e
Marian Schubert <marian.schubert@gmail.com>
2018-04-07 20:13:08 +0200

    Make lnd startup scripts more readable

--- scripts/runlnd2_testnet.sh
+++ scripts/runlnd2_testnet.sh
@@ -7,4 +7,19 @@ export PATH=$PATH:/mnt/data/lnd/lndbin
 #--restlisten=                                           Add an interface/port to listen for REST connections
 #--listen=                                               Add an interface/port to listen for peer connections
 
-/mnt/data/lnd/lndbin/lnd --lnddir=/mnt/data/lnd/data2_testnet --logdir=/mnt/data/lnd/logs2 --alias=bilum2 --color=#00FF00 --bitcoin.active --bitcoin.node=bitcoind --bitcoin.testnet --bitcoind.dir=/mnt/data/bitcoin/data_testnet/ --bitcoind.rpcuser=biluminate --bitcoind.rpcpass=biluminate --bitcoind.zmqpath=tcp://127.0.0.1:28332 --externalip=35.204.151.181:9376 --listen=127.0.0.1:9736 --rpclisten=127.0.0.1:19736 --restlisten=127.0.0.1:29736
+/mnt/data/lnd/lndbin/lnd \
+    --lnddir=/mnt/data/lnd/data2_testnet \
+    --logdir=/mnt/data/lnd/logs2 \
+    --alias=bilum2 \
+    --color=#00FF00 \
+    --bitcoin.active \
+    --bitcoin.node=bitcoind \
+    --bitcoin.testnet \
+    --bitcoind.dir=/mnt/data/bitcoin/data_testnet/ \
+    --bitcoind.rpcuser=biluminate \
+    --bitcoind.rpcpass=biluminate \
+    --bitcoind.zmqpath=tcp://127.0.0.1:28332 \
+    --externalip=35.204.151.181:9376 \
+    --listen=127.0.0.1:9736 \
+    --rpclisten=127.0.0.1:19736 \
+    --restlisten=127.0.0.1:29736
--- scripts/runlnd_testnet.sh
+++ scripts/runlnd_testnet.sh
@@ -3,4 +3,19 @@
 export PATH=$PATH:/mnt/data/bitcoin/bitcoinbin/bin
 export PATH=$PATH:/mnt/data/lnd/lndbin
 
-/mnt/data/lnd/lndbin/lnd --lnddir=/mnt/data/lnd/data1_testnet --logdir=/mnt/data/lnd/logs1 --alias=bilum1 --color=#FF00FF --bitcoin.active --bitcoin.node=bitcoind --bitcoin.testnet --bitcoind.dir=/mnt/data/bitcoin/data_testnet/ --bitcoind.rpcuser=biluminate --bitcoind.rpcpass=biluminate --bitcoind.zmqpath=tcp://127.0.0.1:28332 --externalip=35.204.151.181 --autopilot.active --autopilot.maxchannels=20 --autopilot.allocation=0.7
+/mnt/data/lnd/lndbin/lnd \
+    --lnddir=/mnt/data/lnd/data1_testnet \
+    --logdir=/mnt/data/lnd/logs1 \
+    --alias=bilum1 \
+    --color=#FF00FF \
+    --bitcoin.active \
+    --bitcoin.node=bitcoind \
+    --bitcoin.testnet \
+    --bitcoind.dir=/mnt/data/bitcoin/data_testnet/ \
+    --bitcoind.rpcuser=biluminate \
+    --bitcoind.rpcpass=biluminate \
+    --bitcoind.zmqpath=tcp://127.0.0.1:28332 \
+    --externalip=35.204.151.181 \
+    --autopilot.active \
+    --autopilot.maxchannels=20 \
+    --autopilot.allocation=0.7
