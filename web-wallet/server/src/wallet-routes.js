19795d71e970e011ec2568e5b39d9be8c44f895d
Marian Schubert <marian.schubert@gmail.com>
2018-04-17 19:33:07 +0200

    Convert wallet-routes.js to async/await

--- web-wallet/server/src/wallet-routes.js
+++ web-wallet/server/src/wallet-routes.js
@@ -3,8 +3,10 @@ const jwtCheck = require('./jwt-check')
 const db = require('./db')
 
 const router = express.Router()
-router.get('/info', jwtCheck, (req, res) => {
-  return db.getOrCreateWallet(req.user).then(wallet => res.send(wallet))
+
+router.get('/info', jwtCheck, async (req, res) => {
+  const wallet = await db.getOrCreateWallet(req.user)
+  res.send(wallet)
 })
 
 module.exports = router
