bbff8b6b0695fbda5de347f8aaf04a7c59276896
Marian Schubert <marian.schubert@gmail.com>
2018-05-10 23:24:46 +0200

    Remove cache_static_manifest from prod.exs

--- backend/apps/wallet_api/config/prod.exs
+++ backend/apps/wallet_api/config/prod.exs
@@ -15,8 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :wallet_api, WalletApiWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "example.com", port: 80]
 
 config :wallet_api, WalletApi.Lightning,
   lnd_base_url: System.get_env("LND_REST_URL"),
