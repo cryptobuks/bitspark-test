3ddebf0f5fe0a49a1c087654d672f61684ccc854
Marian Schubert <marian.schubert@gmail.com>
2018-05-16 20:48:37 +0200

    Remove raw transaction :create handler
    Raw transaction records should only be created by the system (e.g., by
    processing lightning payment).
    
    Also remove transaction controller test. Current functionality is exercised by
    features/payment_test.exs

--- backend/apps/wallet_api/lib/wallet_api_web/controllers/transaction_controller.ex
+++ backend/apps/wallet_api/lib/wallet_api_web/controllers/transaction_controller.ex
@@ -29,16 +29,6 @@ defmodule WalletApiWeb.TransactionController do
     end
   end
 
-  def create(conn, %{"transaction" => transaction_params}) do
-    with {:ok, %Transaction{} = transaction} <- Wallets.create_transaction(
-           transaction_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", transaction_path(conn, :show, transaction))
-      |> render("show.json", transaction: transaction)
-    end
-  end
-
   def show(conn, %{"id" => id}) do
     transaction = Wallets.get_transaction!(id)
     render(conn, "show.json", transaction: transaction)
