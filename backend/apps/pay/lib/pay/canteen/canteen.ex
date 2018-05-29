c7df50e52f5b42c9a79b902ed9fdad2572e8c451
Marian Schubert <marian.schubert@gmail.com>
2018-05-29 08:00:33 +0200

    Create Pay.Canteen module

---
+++ backend/apps/pay/lib/pay/canteen/canteen.ex
@@ -0,0 +1,5 @@
+defmodule Pay.Canteen do
+  def create_invoice(:baguette) do
+    "lntb1500n1pdscxv9pp5khljzedum3rlj9l07axd360hkwpvwrtvyejyhwakgwmwx4eyxemqdpa2fjkzep6yptksct5ypc8ymmzd3jk6ueqv3hjq7t0w5sxsctkv5s8w6r9dcs8qcqzysqhsjze7ze8pers05g65jptqzepekv0cc4eafky43eskgu9n3qw2kxefh8qfr5yfd2gtx5fycwmvex39jy8rj9y6m2ana7pewggr653sq0zvxyq"
+  end
+end
--- backend/apps/pay_web/lib/pay_web/controllers/baguette_invoice_controller.ex
+++ backend/apps/pay_web/lib/pay_web/controllers/baguette_invoice_controller.ex
@@ -2,7 +2,8 @@ defmodule PayWeb.BaguetteInvoiceController do
   use PayWeb, :controller
 
   def index(conn, _params) do
-    invoice = "lntb1500n1pdscxv9pp5khljzedum3rlj9l07axd360hkwpvwrtvyejyhwakgwmwx4eyxemqdpa2fjkzep6yptksct5ypc8ymmzd3jk6ueqv3hjq7t0w5sxsctkv5s8w6r9dcs8qcqzysqhsjze7ze8pers05g65jptqzepekv0cc4eafky43eskgu9n3qw2kxefh8qfr5yfd2gtx5fycwmvex39jy8rj9y6m2ana7pewggr653sq0zvxyq"
+    invoice = Pay.Canteen.create_invoice(:baguette)
+
     conn =
       conn
       |> put_resp_header("lightning-invoice", invoice)
