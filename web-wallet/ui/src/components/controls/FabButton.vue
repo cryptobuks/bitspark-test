1ce44d47801178f8e33d0a9a605f949ade72ddb2
Josef Doskar <Doskar.jd@gmail.com>
2018-08-09 20:08:38 +0200

    Prototype of Send/Recieve Button

--- web-wallet/ui/src/components/controls/FabButton.vue
+++ web-wallet/ui/src/components/controls/FabButton.vue
@@ -1,7 +1,19 @@
 <template>
-  <v-btn v-if="this.user" color="yellow" fixed bottom right fab @click="handleOnCLick()">
-    <v-icon>add</v-icon>
-  </v-btn>  
+  <v-speed-dial fixed v-model="fab" :bottom="true" :right="true" :direction="'top'" :transition="'scale-transition'">
+      <v-btn color="yellow" slot="activator" v-model="fab" fab>
+        <v-icon>add</v-icon>
+        <v-icon>close</v-icon>
+      </v-btn>
+      <v-btn fab dark small color="green"> 
+        <v-icon class="bi-icon-send">arrow_upward</v-icon>
+      </v-btn>
+      <v-btn fab dark small color="red">
+        <v-icon class="bi-icon-recieve">arrow_upward</v-icon>
+      </v-btn>
+      <v-btn fab dark small color="red">
+        <v-icon class="bi-icon-recieve">arrow_upward</v-icon>
+      </v-btn>
+    </v-speed-dial>
 </template>
 
 <script>
@@ -23,5 +35,11 @@ export default {
 <style>
 .btn--bottom.btn--absolute {
   bottom: 16px;
+}
+.bi-icon-send {
+  transform: rotate(225deg);
+}
+.bi-icon-recieve {
+  transform: rotate(45deg);
 }
 </style>
