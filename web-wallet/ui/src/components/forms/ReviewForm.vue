6bcc3ab03aa324c9baea97c877ef4c6c9e3e7ee4
Josef Doskar <Doskar.jd@gmail.com>
2018-08-19 21:09:51 +0200

    ReviewForm

---
+++ web-wallet/ui/src/components/forms/ReviewForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-form class="bi-form">
+    <v-flex xs12 text-xs-right>
+      <h1>ReviewForm</h1>
+    </v-flex>
+    <BottomButton :label="'Send'" :onClick="handleSendClick" />
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'ReviewForm'
+}
+</script>
+
+<style scoped>
+.bi-form {
+  width: 100%;
+  padding: 0px 22px;
+}
+</style>
--- web-wallet/ui/src/components/pages/Send.vue
+++ web-wallet/ui/src/components/pages/Send.vue
@@ -1,26 +1,27 @@
 <template>
   <v-container class="bi-container" fluid>
     <v-layout align-center justify-center>
-      <SendForm />
-      <BottomButton :label="'Continue to Review'" :disabled="true" :onClick="handleReviewClick"/>
+      <ReviewForm v-if="showForm ==='review'" :payment="payment"/>
+      <SendForm v-else />
     </v-layout>
   </v-container>
 </template>
 
 <script>
-import BottomButton from '@/components/controls/BottomButton'
 import SendForm from '@/components/forms/SendForm'
+import ReviewForm from '@/components/forms/ReviewForm'
 
 export default {
   name: 'Send',
+  data () {
+    return {
+      showForm: 'send',
+      payment: {}
+    }
+  },
   components: {
     SendForm,
-    BottomButton
-  },
-  methods: {
-    handleReviewClick () {
-      console.log('Handle review click.')
-    }
+    ReviewForm
   }
 }
 </script>
