dc1765ed0de79d65a52079bc0f72287917ef0068
Josef Doskar <Doskar.jd@gmail.com>
2018-08-19 22:26:20 +0200

    Basic info and styling of ReviewForm

--- web-wallet/ui/src/components/forms/ReviewForm.vue
+++ web-wallet/ui/src/components/forms/ReviewForm.vue
@@ -1,22 +1,71 @@
 <template>
-  <v-form class="bi-form">
-    <v-flex xs12 text-xs-right>
-      <h1>ReviewForm</h1>
-      {{payment}}
-    </v-flex>
+  <v-layout align-center justify-space-between column fill-height fill-width>
+    <div class="bi-contacts-container">
+      <v-flex xs12>
+        <v-list>
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <v-icon v-if="!this.user.picture" x-large class="bi-icon">account_circle</v-icon>
+              <img v-else v-bind:src="this.user.picture" />
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title class="bi-name">{{this.user.name}} (You)</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-flex>
+      <v-flex xs12>
+        <v-list>
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <v-icon x-large class="bi-icon">account_circle</v-icon>
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title class="bi-name">{{payment.sendTo}}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-flex>
+    </div>
+    <div class="bi-amounts-container">
+      <v-container pa-0 fluid>
+        <v-layout row>
+          <v-flex xs12 text-xs-center>
+            <span>TOTAL (FEES INCLUDED)</span><br>
+            <Amount :msatoshi="totalAmount" :medium="true" />
+          </v-flex>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs6 text-xs-center>
+            <span>FEE INCLUDED</span>
+          </v-flex>
+          <v-flex xs6 text-xs-center>
+            <span>TOTAL FIAT VALUE</span>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </div>
     <BottomButton :label="'Send'" :onClick="handleSendClick" />
-  </v-form>
+  </v-layout>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import BottomButton from '@/components/controls/BottomButton'
+import Amount from '@/components/Amount'
 
 export default {
   name: 'ReviewForm',
   components: {
-    BottomButton
+    BottomButton,
+    Amount
   },
-  props: ['payment'],
+  computed: {
+    ...mapGetters(['user', 'payment']),
+    totalAmount () {
+      return this.payment.amount * 100000000000
+    }
+  },
   methods: {
     handleSendClick () {
       console.log('handleSendClick')
@@ -26,8 +75,29 @@ export default {
 </script>
 
 <style scoped>
+span {
+  font-size: 11px;
+}
 .bi-form {
   width: 100%;
   padding: 0px 22px;
+}
+.bi-contacts-container {
+  width: 100%;
+  margin-top: -50px;
+  padding: 15px;
+  background-color: #fafafa;
+}
+.bi-amounts-container {
+  width: 100%;
+  padding: 15px;
+  background-color: #fafafa;
+  margin-bottom: 56px;
+}
+.bi-contacts-container ul {
+  background-color: #fafafa;
+}
+.bi-contacts-container i {
+  font-size: 45px !important;
 }
 </style>
--- web-wallet/ui/src/components/pages/Review.vue
+++ web-wallet/ui/src/components/pages/Review.vue
@@ -1,8 +1,6 @@
 <template>
   <v-container class="bi-container" fluid>
-    <v-layout align-center justify-center>
-      <ReviewForm />
-    </v-layout>
+    <ReviewForm />
   </v-container>
 </template>
 
@@ -17,5 +15,5 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 </style>
