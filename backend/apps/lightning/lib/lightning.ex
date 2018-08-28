a4eab8e2c7a5963a8e72461e76b80d5d64809ae3
Marian Schubert <marian.schubert@gmail.com>
2018-08-28 21:36:05 +0200

    Extract low level REST API client code from Lightning

--- backend/apps/lightning/lib/lightning.ex
+++ backend/apps/lightning/lib/lightning.ex
@@ -1,50 +1,14 @@
 defmodule Lightning do
   @moduledoc """
-  The Lnd context.
-
-  LND REST API: https://api.lightning.community/rest/index.html
+  Lightning functions.
   """
-  use Tesla
-  plug Tesla.Middleware.JSON, engine: Poison
-  plug Tesla.Middleware.Timeout, timeout: 120_000 #ms
-  plug Tesla.Middleware.Logger
 
+  alias Lightning.LndRest
   alias Lightning.Invoice
 
-  def client(config) do
-    base_url = Keyword.get(config, :lnd_base_url)
-    macaroon = Keyword.get(config, :lnd_macaroon)
-
-    Tesla.build_client([
-      {Tesla.Middleware.BaseUrl, base_url},
-      {Tesla.Middleware.Headers, [{"grpc-metadata-macaroon", macaroon}]}
-    ])
-  end
-
-  @doc """
-  Issue GET request to given LND.
-
-      Wallet.lightning_config |> Lightning.xget!("/v1/getinfo")
-
-  """
-  def xget!(config, path) do
-    client(config)
-    |> get!(path)
-  end
-
-  @doc """
-  Issue POST request to given LND.
-
-      Wallet.lightning_config |> Lightning.xpost!("/v1/invoices", %{memo: "...description", value: 12345, receipt: Base.encode64("...receipt")})
-
-  """
-  def xpost!(config, path, data) do
-    client(config)
-    |> post!(path, data)
-  end
-
   def decode_invoice(config, invoice) do
-    %{body: body} = client(config) |> get!("/v1/payreq/#{invoice}")
+    %{body: body} = LndRest.xget!(config, "/v1/payreq/#{invoice}")
+
     changeset = Lightning.Invoice.changeset(
       %Invoice{},
       %{
@@ -72,14 +36,14 @@ defmodule Lightning do
       "/v1/invoices"
     end
 
-    %{body: body} = client(config) |> get!(url)
+    %{body: body} = LndRest.xget!(config, url)
     body |> Map.get("invoices")
   end
 
   def get_node_info(config, node_pub_key) do
     %{body: body} =
-      client(config)
-      |> get!("/v1/graph/node/#{node_pub_key}")
+      LndRest.xget!(config, "/v1/graph/node/#{node_pub_key}")
+
     case body do
       %{"node" => node} ->
         {:ok, %{alias: node["alias"]}}
@@ -89,8 +53,9 @@ defmodule Lightning do
 
   def create_invoice(config, %{description: description, msatoshi: msatoshi}) do
     %{body: body} =
-      client(config)
-      |> post!("/v1/invoices", %{memo: description, value: Bitcoin.to_satoshi({msatoshi, :msatoshi})})
+      LndRest.xpost!(
+        config, "/v1/invoices", %{memo: description, value: Bitcoin.to_satoshi({msatoshi, :msatoshi})})
+
     case body do
       %{"payment_request" => invoice} -> {:ok, invoice}
       _ -> {:error, body}
@@ -98,7 +63,9 @@ defmodule Lightning do
   end
 
   def pay_invoice(config, invoice) do
-    %{body: body} = client(config) |> post!("/v1/channels/transactions", %{payment_request: invoice})
+    %{body: body} = LndRest.xpost!(
+      config, "/v1/channels/transactions", %{payment_request: invoice})
+
     case body do
       %{"payment_preimage" => _} -> {:ok, body}
       _ -> {:error, body}
