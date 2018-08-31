e7708eff210244f827ad5d7ed34c00ce0745d9a6
Marian Schubert <marian.schubert@gmail.com>
2018-08-31 08:52:49 +0200

    Remove legacy JavaScript backend code

--- web-wallet/server/migrate.js
+++
@@ -1,5 +0,0 @@
-const config = require('./src/config.js')
-
-process.env.DATABASE_URL = config.DATABASE_URL
-
-require('./node_modules/node-pg-migrate/bin/node-pg-migrate')
--- web-wallet/server/src/config.js
+++ web-wallet/server/src/config.js
@@ -1,23 +1,2 @@
 // HTTP Server
 exports.httpLogFormat = process.env.HTTP_LOG_FORMAT
-
-// LND
-exports.lndRestUrl = process.env.WALLET_LND_REST_URL
-exports.lndRestMacaroon = process.env.WALLET_LND_REST_MACAROON
-
-// Database
-var db = {
-  user: process.env.PGUSER || 'postgres',
-  database: process.env.PGDATABASE || 'postgres',
-  password: process.env.PGPASSWORD,
-  host: process.env.PGHOST || 'localhost',
-  port: process.env.PGPORT || 5432,
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000 // how long to keep idle client
-}
-
-exports.db = db
-
-exports.DATABASE_URL = 'postgres://' +
-  db.user + (db.password ? (':' + db.password) : '') +
-  '@' + db.host + ':' + db.port + '/' + db.database
