636342c6a8a165d3a93294b56fedde851ec03f81
Doškář Josef <josef.doskar@avast.com>
2018-09-04 22:39:03 +0200

    Prevent double click on BottomButton

--- web-wallet/ui/src/components/controls/BottomButton.vue
+++ web-wallet/ui/src/components/controls/BottomButton.vue
@@ -1,12 +1,17 @@
 <template>
-  <v-btn class="bi-bottom-button primary charcoalGrey--text" v-if="!this.bigScreen" large v-bind:class="{ 'disabled': disabled }" @click="handleOnClick" :tabindex="tabindex" block>{{label}}</v-btn>
-  <v-btn class="bi-bottom-button-big-screen charcoalGrey--text primary" mt-5 v-else large v-bind:class="{ 'disabled': disabled }" @click="handleOnClick" :tabindex="tabindex">{{label}}</v-btn>
+  <v-btn class="bi-bottom-button primary charcoalGrey--text" v-if="!this.bigScreen" large v-bind:class="{ 'disabled': actualDisabled }" @click="handleOnClick" :tabindex="tabindex" block>{{label}}</v-btn>
+  <v-btn class="bi-bottom-button-big-screen charcoalGrey--text primary" mt-5 v-else large v-bind:class="{ 'disabled': actualDisabled }" @click="handleOnClick" :tabindex="tabindex">{{label}}</v-btn>
 </template>
 
 <script>
 export default {
   name: 'BottomButton',
   props: ['label', 'onClick', 'disabled', 'tabindex'],
+  data () {
+    return {
+      actualDisabled: this.disabled
+    }
+  },
   computed: {
     bigScreen () {
       return this.$vuetify.breakpoint.name === 'lg' || this.$vuetify.breakpoint.name === 'xl' || this.$vuetify.breakpoint.name === 'md'
@@ -14,9 +19,18 @@ export default {
   },
   methods: {
     handleOnClick () {
-      if (!this.disabled) {
+      if (!this.actualDisabled) {
+        this.actualDisabled = true
         this.onClick()
+        setTimeout(function () {
+          this.actualDisabled = false
+        }.bind(this), 300)
       }
+    }
+  },
+  watch: {
+    disabled (newVal) {
+      this.actualDisabled = newVal
     }
   }
 }
