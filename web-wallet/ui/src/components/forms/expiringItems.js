e4c78e18a8eafd8bb640c37d622eea529aa83c34
Marian Schubert <marian.schubert@gmail.com>
2018-09-23 18:54:44 +0200

    Get rid of artificial ID from expiringItems
    Just use expires after which is unique.

--- web-wallet/ui/src/components/forms/expiringItems.js
+++ web-wallet/ui/src/components/forms/expiringItems.js
@@ -1,19 +1,24 @@
-const items = [
-  {
-    id: 1,
-    value: 3600,
-    text: 'Expiring in one hour'
-  },
-  {
-    id: 2,
-    value: 86400,
-    text: 'Expiring in 24 hrs'
-  },
-  {
-    id: 3,
-    value: 604800,
-    text: 'Expiring in one week'
+function createItem ({ expiresAfter, text }) {
+  return {
+    id: expiresAfter,
+    value: expiresAfter,
+    text
   }
+}
+
+const items = [
+  createItem({
+    expiresAfter: 3600,
+    text: 'Expiring in one hour'
+  }),
+  createItem({
+    expiresAfter: 86400,
+    text: 'Expiring in 24 hrs'
+  }),
+  createItem({
+    expiresAfter: 604800,
+    text: 'Expiring in one week'
+  })
 ]
 
 export default items
