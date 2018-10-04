0711dfbbccab2eae80b5329b27243d4525f7bbc3
Marian Schubert <marian.schubert@gmail.com>
2018-10-04 09:57:58 +0200

    Extract wallets GraphQL stuff into Wallet.Schema.Wallets

--- backend/apps/wallet/lib/wallet/schema/schema.ex
+++ backend/apps/wallet/lib/wallet/schema/schema.ex
@@ -1,6 +1,5 @@
 defmodule Wallet.Schema do
   use Absinthe.Schema
-  require Logger
 
   alias Wallet.Accounts
   alias Wallet.Wallets
@@ -8,32 +7,15 @@ defmodule Wallet.Schema do
   import_types Absinthe.Type.Custom
   import_types Wallet.Schema.Scalars
   import_types Wallet.Schema.Enums
+  import_types Wallet.Schema.Wallets
   import_types Wallet.Schema.Transactions
-
-  object :wallet_balance do
-    @desc "Amount in milli-satoshi (0.001 satoshi)."
-    field :msatoshi, :msatoshi
-  end
-
-  object :wallet do
-    field :id, non_null(:id)
-    @desc """
-    Spendable balance
-
-    - Includes pending outgoing transactions (e.g., non-claimed email transactions)
-    - Doesn't include pending incomming transactions (e.g., unconfirmed on-chain transactions)
-    """
-    field :balance, non_null(:wallet_balance), resolve: &get_wallet_balance/3
-    @desc "List of transactions associated with the wallet."
-    field :transactions, list_of(:transaction), resolve: &find_transactions/3
-  end
 
   query do
     @desc """
     Wallet of currently authenticated user (via JWT token).
     """
     field :current_user_wallet, non_null(:wallet) do
-      resolve &get_wallet/3
+      resolve &Wallet.Schema.Wallets.get_wallet/3
     end
   end
 
@@ -59,28 +41,7 @@ defmodule Wallet.Schema do
         {:ok, topic: wallet.id}
       end
 
-      resolve &get_wallet/3
+      resolve &Wallet.Schema.Wallets.get_wallet/3
     end
-  end
-
-  def get_wallet(%Wallets.Transaction{wallet_id: wallet_id}, _args, _context) do
-    wallet = Wallets.get_wallet!(wallet_id)
-    {:ok, wallet}
-  end
-
-  def get_wallet(_root, _args, %{context: context}) do
-    account = Accounts.login!(context.assigns.joken_claims["sub"])
-    wallet = Wallets.get_or_create_wallet!(account)
-    {:ok, wallet}
-  end
-
-  def get_wallet_balance(wallet, _args, _info) do
-    balance = Wallets.get_wallet_balance(wallet)
-    {:ok, %{msatoshi: balance}}
-  end
-
-  def find_transactions(wallet, _args, _info) do
-    transactions = Wallets.list_transactions(wallet)
-    {:ok, transactions}
   end
 end
---
+++ backend/apps/wallet/lib/wallet/schema/wallets.ex
@@ -0,0 +1,48 @@
+defmodule Wallet.Schema.Wallets do
+  @moduledoc false
+  use Absinthe.Schema.Notation
+
+  require Logger
+  alias Wallet.Accounts
+  alias Wallet.Wallets
+
+  object :wallet_balance do
+    @desc "Amount in milli-satoshi (0.001 satoshi)."
+    field :msatoshi, :msatoshi
+  end
+
+  object :wallet do
+    field :id, non_null(:id)
+    @desc """
+    Spendable balance
+
+    - Includes pending outgoing transactions (e.g., non-claimed email transactions)
+    - Doesn't include pending incomming transactions (e.g., unconfirmed on-chain transactions)
+    """
+    field :balance, non_null(:wallet_balance), resolve: &get_wallet_balance/3
+    @desc "List of transactions associated with the wallet."
+    field :transactions, list_of(:transaction), resolve: &find_transactions/3
+  end
+
+  ## Resolvers
+  def get_wallet(%Wallets.Transaction{wallet_id: wallet_id}, _args, _context) do
+    wallet = Wallets.get_wallet!(wallet_id)
+    {:ok, wallet}
+  end
+
+  def get_wallet(_root, _args, %{context: context}) do
+    account = Accounts.login!(context.assigns.joken_claims["sub"])
+    wallet = Wallets.get_or_create_wallet!(account)
+    {:ok, wallet}
+  end
+
+  def get_wallet_balance(wallet, _args, _info) do
+    balance = Wallets.get_wallet_balance(wallet)
+    {:ok, %{msatoshi: balance}}
+  end
+
+  def find_transactions(wallet, _args, _info) do
+    transactions = Wallets.list_transactions(wallet)
+    {:ok, transactions}
+  end
+end
