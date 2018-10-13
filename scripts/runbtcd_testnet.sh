cf1044dc224d1dccbd157c299a5a06b93bd8c763
Marian Schubert <marian.schubert@gmail.com>
2018-10-13 14:32:17 +0200

    Reformat runbtcd_testnet.sh script

--- scripts/runbtcd_testnet.sh
+++ scripts/runbtcd_testnet.sh
@@ -3,4 +3,15 @@
 export PATH=$PATH:/mnt/data/bitcoin/bitcoinbin/bin
 export PATH=$PATH:/mnt/data/lightning/lightning/cli
 
-/mnt/data/bitcoin/bitcoinbin/bin/bitcoind -testnet -datadir=/mnt/data/bitcoin/data_testnet -minrelaytxfee=0.000001 -server=1 -daemon=1 -par=2 -txindex=1 -rpcuser=biluminate -rpcpassword=biluminate --zmqpubrawblock=tcp://127.0.0.1:28332 --zmqpubrawtx=tcp://127.0.0.1:28333
+/mnt/data/bitcoin/bitcoinbin/bin/bitcoind \
+    -testnet \
+    -datadir=/mnt/data/bitcoin/data_testnet \
+    -minrelaytxfee=0.000001 \
+    -server=1 \
+    -daemon=1 \
+    -par=2 \
+    -txindex=1 \
+    -rpcuser=biluminate \
+    -rpcpassword=biluminate \
+    --zmqpubrawblock=tcp://127.0.0.1:28332 \
+    --zmqpubrawtx=tcp://127.0.0.1:28333
